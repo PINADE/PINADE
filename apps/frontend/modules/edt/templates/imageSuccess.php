694a597a8445bdd0e957fde132099eee89298195
t.helleboid <t.helleboid@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-02 17:01:17 +0000

    Correction lien -now
    git-svn-id: https://svn.iariss.fr/edt@48 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/modules/edt/templates/imageSuccess.php
+++ apps/frontend/modules/edt/templates/imageSuccess.php
@@ -1,6 +1,6 @@
 <p>
   <?php echo link_to('semaine précédente', "@image?filiere=$filiere&promo=$promo&semaine=$semaine_precedente") ?> - 
-  <?php echo link_to('semaine actuelle', "@image_default?filiere=$filiere&promo=$promo") ?> - 
+  <?php echo link_to('semaine actuelle', "@image?filiere=$filiere&promo=$promo&semaine=") ?> - 
   <?php echo link_to('semaine suivante', "@image?filiere=$filiere&promo=$promo&semaine=$semaine_suivante") ?>
 </p>
 <img src='<?php echo $image_path ?>' alt='emploi du temps <?php echo $filiere." ".$promo ?>'/>
