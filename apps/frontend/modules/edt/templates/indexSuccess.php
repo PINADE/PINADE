4b6e0924c8cc7e1c0d09389fe863e07f1a09e388
m.mure <m.mure@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-02 15:23:51 +0000

    amélioration du menu, placement + icone
    git-svn-id: https://svn.iariss.fr/edt@32 81824da9-04c7-4f20-bb1d-81339a13f318

---
+++ apps/frontend/modules/edt/templates/indexSuccess.php
@@ -0,0 +1,16 @@
+<p>
+  Bienvenue sur l'emploi du temps de l'Ensisa.
+</p>
+
+<p>
+  Selectionnez votre filière:
+</p>
+
+<ul>
+  <li><?php echo link_to('Informatique', '/info') ?></li>
+  <li><?php echo link_to('Automatique', '/auto') ?></li>
+  <li><?php echo link_to('Textile', '/text') ?></li>
+  <li><?php echo link_to('Mécanique', '/meca') ?></li>
+  <li><?php echo link_to('Système de production', '/prod') ?></li>
+</ul>
+
--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -21,35 +21,50 @@
         <div id="navigation">
           <div id="menu">
             <ul>
-              <li>Informatique</li>
-              <li><?php echo link_to('1A', 'info/1A') ?></li>
-              <li><?php echo link_to('2A', 'info/2A') ?></li>
-              <li><?php echo link_to('3A', 'info/3A') ?></li>
+              <li><?php echo image_tag('logos/info.png', 'alt=') ?>Informatique
+                <ul>
+                  <li><?php echo link_to('1A', 'info/1A') ?></li>
+                  <li><?php echo link_to('2A', 'info/2A') ?></li>
+                  <li><?php echo link_to('3A', 'info/3A') ?></li>
+                </ul>
+              </li>
+              
+              <li><?php echo image_tag('logos/auto.png', 'alt=') ?>Automatique
+                <ul>
+                  <li><?php echo link_to('1A', 'auto/1A') ?></li>
+                  <li><?php echo link_to('2A', 'auto/2A') ?></li>
+                  <li><?php echo link_to('3A', 'auto/3A') ?></li>
+                </ul>
+              </li>
 
-              <li>Automatique</li>
-              <li><?php echo link_to('1A', 'auto/1A') ?></li>
-              <li><?php echo link_to('2A', 'auto/2A') ?></li>
-              <li><?php echo link_to('3A', 'auto/3A') ?></li>
+              <li><?php echo image_tag('logos/text.png', 'alt=') ?>Textile
+                <ul>
+                  <li><?php echo link_to('1A', 'text/1A') ?></li>
+                  <li><?php echo link_to('2A', 'text/2A') ?></li>
+                  <li><?php echo link_to('3A', 'text/3A') ?></li>
+                </ul>
+              </li>
 
-              <li>Textile</li>
-              <li><?php echo link_to('1A', 'text/1A') ?></li>
-              <li><?php echo link_to('2A', 'text/2A') ?></li>
-              <li><?php echo link_to('3A', 'text/3A') ?></li>
+              <li><?php echo image_tag('logos/meca.png', 'alt=') ?>Mécanique
+                <ul>
+                  <li><?php echo link_to('1A', 'meca/1A') ?></li>
+                  <li><?php echo link_to('2A', 'meca/2A') ?></li>
+                  <li><?php echo link_to('3A', 'meca/3A') ?></li>
+                </ul>
+              </li>
 
-              <li>Mécanique</li>
-              <li><?php echo link_to('1A', 'meca/1A') ?></li>
-              <li><?php echo link_to('2A', 'meca/2A') ?></li>
-              <li><?php echo link_to('3A', 'meca/3A') ?></li>
-
-              <li>Système de production</li>
-              <li><?php echo link_to('1A', 'prod/1A') ?></li>
-              <li><?php echo link_to('2A', 'prod/2A') ?></li>
-              <li><?php echo link_to('3A', 'prod/3A') ?></li>
+              <li><?php echo image_tag('logos/prod.png', 'alt=') ?>Système de production
+                <ul>
+                  <li><?php echo link_to('1A', 'prod/1A') ?></li>
+                  <li><?php echo link_to('2A', 'prod/2A') ?></li>
+                  <li><?php echo link_to('3A', 'prod/3A') ?></li>
+                </ul>
+              </li>
             </ul>
           </div>
           
           <div id="pub">
-            bzdjmbdzmzd
+            Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié.
           </div>
         </div>
         
