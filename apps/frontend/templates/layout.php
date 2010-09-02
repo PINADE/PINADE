3b22504529f01c5c01c72a022cb832f7e8319427
m.mure <m.mure@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-02 15:58:54 +0000

    espace pub IARISS
    git-svn-id: https://svn.iariss.fr/edt@42 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -64,7 +64,8 @@
           </div>
           
           <div id="pub">
-            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié.
+            <p>Tu es étudiant à l'ENSISA ? Tu souhaites rejoindre une équipe motivée et acquérir de nombreuses compétences ? IARISS recrute, dès la rentrée !
+Pour faciliter ta candidature, tu peux postuler sur un site dédié : <?php echo link_to('Candidature IARISS', 'http://candidature.iariss.fr') ?>
             </p>
           </div>
         </div>
