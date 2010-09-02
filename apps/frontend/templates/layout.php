827f8bd70fcfe4c671380eaea13b1edd7a996155
m.mure <m.mure@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-02 18:12:17 +0000

    ajout lien accueil
    git-svn-id: https://svn.iariss.fr/edt@55 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -22,6 +22,9 @@
         <div id="navigation">
           <div id="menu">
             <ul>
+              <li>
+                <?php echo link_to('Accueil', '/', "inline") ?>
+              </li>
 <?php foreach(array(
   "info" => "Informatique",
   "auto" => "Automatique",
@@ -58,14 +61,15 @@ Pour faciliter ta candidature, tu peux postuler sur un site dédié : <?php echo
 
     <!-- Piwik -->
     <script type="text/javascript">
-    var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.iariss.fr/" : "http://piwik.iariss.fr/");
-    document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-    </script><script type="text/javascript">
-    try {
-    var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 9);
-    piwikTracker.trackPageView();
-    piwikTracker.enableLinkTracking();
-    } catch( err ) {}
+      var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.iariss.fr/" : "http://piwik.iariss.fr/");
+      document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+    </script>
+    <script type="text/javascript">
+      try {
+        var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 9);
+        piwikTracker.trackPageView();
+        piwikTracker.enableLinkTracking();
+      } catch( err ) {}
     </script><noscript><p><img src="http://piwik.iariss.fr/piwik.php?idsite=9" style="border:0" alt="" /></p></noscript>
     <!-- End Piwik Tag -->
   </body>
