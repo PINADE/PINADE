39fcdab219d4e0f7e41cfd8226c5da7d3139f079
t.helleboid <t.helleboid@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-02 11:55:37 +0000

    Ajout doc + tools
    git-svn-id: https://svn.iariss.fr/edt@11 81824da9-04c7-4f20-bb1d-81339a13f318

---
+++ lib/Tools.class.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * edt tools.
+ *
+ * @package    edt
+ * @subpackage edt
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class AdeTools
+{
+  
+  public function getAdeCookie() {
+    /* Get lt for login */
+    $login_page = file_get_contents("https://cas.uha.fr/cas/login");
+    $pattern = '@name="lt" value="([^"]+)" />@';
+    preg_match($pattern, $login_page, $matches);
+    $lt = $matches[1];
+
+    // base 64 :
+    // username=prenom.nom%40uha.fr&password=YOURPASSWORD
+
+    /* Get CAS Cookie and link to emploidutemps.uha.fr */
+    $data_string = base64_decode("dXNlcm5hbWU9dGhlb3BoaWxlLmhlbGxlYm9pZCU0MHVoYS5mciZwYXNzd29yZD1iYWxhbXVydWdhbg==")."&lt=$lt";
+    $handle = curl_init("https://cas.uha.fr/cas/login?service=http%3A%2F%2Femploidutemps.uha.fr%2Fade%2Fstandard%2Fgui%2Finterface.jsp");
+    curl_setopt($handle, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded')); 
+    curl_setopt($handle, CURLOPT_POST, true);
+    curl_setopt($handle, CURLOPT_POSTFIELDS, $data_string /* $data */);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.2.7) Gecko/20100701 Firefox/3.6.7");
+    curl_setopt($handle, CURLOPT_REFERER, "https://cas.uha.fr/cas/login");
+    curl_setopt($handle, CURLOPT_HEADER, true);
+//    curl_setopt($handle, CURLOPT_WRITEHEADER, fopen("head.txt", "w+"));
+    $content = curl_exec($handle);
+    /* Get CAS Cookie */
+    $pattern = '@Set-Cookie: CASTGC=([^;]+)@';
+    preg_match($pattern, $content, $matches);
+    $cookie = $matches[1];
+    /* Get ticket */
+    $pattern = '@window.location.href="([^"]+)@';
+    preg_match($pattern, $content, $matches);
+    $link_edt = $matches[1];
+//    print_r($matches);
+
+    $handle = curl_init($link_edt);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($handle, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 6.1; fr; rv:1.9.2.7) Gecko/20100701 Firefox/3.6.7");
+    curl_setopt($handle, CURLOPT_REFERER, "https://cas.uha.fr/cas/login");
+    curl_setopt($handle, CURLOPT_HEADER, true);
+    curl_setopt($handle, CURLOPT_COOKIE, "CASTGC=$cookie");
+    curl_setopt($handle, CURLOPT_FOLLOWLOCATION, true);
+    $content = curl_exec($handle);
+    /* Get ADE cookie  */
+    $pattern = '@Set-Cookie: JSESSIONID=([^;]+)@';
+    preg_match($pattern, $content, $matches);
+    $ade_cookie = $matches[1];
+
+    return $ade_cookie;
+  }
+
+  public function getAdeImage()
+  {
+    
+  }
+}
+
+
