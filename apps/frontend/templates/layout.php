71b40d186ddc439dd3e5010d9fbc3e6ae7af02e9
m.mure <m.mure@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-02 17:33:36 +0000

    correction orthographe
    git-svn-id: https://svn.iariss.fr/edt@52 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -12,7 +12,7 @@
     <div id="global">
       <div id="entete">
         <ul>
-          <li><?php echo link_to('Acceuil', '/') ?></li>
+          <li><?php echo link_to('Accueil', '/') ?></li>
           <li><?php echo link_to('Annales', 'http://annales.iariss.fr/') ?></li>
           <li><?php echo link_to('Trombinoscope', 'http://trombi.iariss.fr/') ?></li>
           <li><?php echo link_to('Blog', 'http://ensisa11.iariss.fr/') ?></li>
