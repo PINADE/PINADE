da1ac0375e8b135b2839938f7bb84655215da247
m.mure <m.mure@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-02 11:18:19 +0000

    ajout module edt vide
    git-svn-id: https://svn.iariss.fr/edt@7 81824da9-04c7-4f20-bb1d-81339a13f318

---
+++ apps/frontend/modules/edt/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * edt actions.
+ *
+ * @package    edt
+ * @subpackage edt
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class edtActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
