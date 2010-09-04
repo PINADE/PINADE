52d849b68df892741466de6a3af9cd805d09be05
m.mure <m.mure@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-04 15:09:44 +0000

    peaufinage graphique
    git-svn-id: https://svn.iariss.fr/edt@95 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/modules/edt/templates/imageSuccess.php
+++ apps/frontend/modules/edt/templates/imageSuccess.php
@@ -8,8 +8,8 @@
 </h2>
 <p class="center">
   <?php echo link_to(image_tag('divers/precedent.png', 'alt=<<')
-, "@image?filiere=$filiere&promo=$promo&semaine=$semaine_precedente") ?> - 
-  <?php echo link_to('semaine actuelle', "@image?filiere=$filiere&promo=$promo&semaine=") ?> - 
+, "@image?filiere=$filiere&promo=$promo&semaine=$semaine_precedente") ?>
+  <?php echo link_to('semaine actuelle', "@image?filiere=$filiere&promo=$promo&semaine=") ?>
   <?php echo link_to(image_tag('divers/suivant.png', 'alt=>>'), "@image?filiere=$filiere&promo=$promo&semaine=$semaine_suivante") ?>
 </p>
 <img src='<?php echo $image_path ?>' alt='emploi du temps <?php echo $filiere." ".$promo ?>'/>
