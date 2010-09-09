ebcf3cb45e3063d9940e6463eb4c6438af9d7ab8
m.mure <m.mure@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-09 19:27:54 +0000

    redirection en page d'accueil si acces au cron par une autre ip que localhost
    git-svn-id: https://svn.iariss.fr/edt@127 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/modules/cron/actions/actions.class.php
+++ apps/frontend/modules/cron/actions/actions.class.php
@@ -12,6 +12,11 @@ class cronActions extends sfActions
 {
   public function executeAll(sfWebRequest $request)
   {
+    if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+    {
+      $request->redirect('@homepage');
+    }
+    
     $filieres = sfConfig::get('sf_filieres');
     $semaine = AdeTools::getSemaineNumber();
 
