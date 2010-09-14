0b8874866f2bd62a9b3c5bcb02ef0e92621ba451
t.helleboid <t.helleboid@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-09-14 00:47:20 +0000

    settings.default: real md5 : easy update with cron add FAQ section
    git-svn-id: https://svn.iariss.fr/edt@149 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/modules/edt/templates/faqSuccess.php
+++ apps/frontend/modules/edt/templates/faqSuccess.php
@@ -4,17 +4,24 @@
   <?php include_slot('title') ?>
 </h1>
 
+<h2 id="fiable">Est-ce le site de référence ?</h2>
+<b>Non, ce site n'est pas le site officiel de l'emploi du temps</b> de l'<?php echo link_to('ENSISA','http://www.ensisa.fr/') ?>.
+Le site officiel est <?php echo link_to('emploidutemps.uha.fr', 'http://emploidutemps.uha.fr/') ?> et reste la référence de votre emploi du temps.<br/>
+Cependant, si tout va bien, <b>edt.iariss.fr</b> met à jour régulièrement (<i>cf</i> section <a href="#delai"><b>Délai</b></a>) les emplois du temps.
+
 <h2 id="origine">Qui est à l'origine de ce site ?</h2>
 L'équipe de <?php echo link_to('IARISS', 'http://www.iariss.fr/') ?> a codé ce site pour vous,
 parce qu'elle trouvait que le site proposé par l'UHA, <?php echo link_to('emploidutemps.uha.fr', 'http://emploidutemps.uha.fr/') ?>, 
 n'est pas du tout pratique.<br/>
-Les auteurs initiaux sont Michaël Muré et Thépophile Helleboid, mais nous en attendons d'autres. Venez nous rejoindre&nbsp;!
+Les auteurs initiaux sont <b>Michaël Muré</b> et <b>Thépophile Helleboid</b>, mais nous en attendons d'autres. Venez nous rejoindre&nbsp;!
 
 <h2 id="contact">Je veux contacter les auteurs, à qui m'adresser ?</h2>
-Envoyez un mail à <code>contact@iariss.fr</code>, c'est l'adresse unique pour contacter les auteurs.<br/>
+Envoyez un mail à <b><code>contact@iariss.fr</code></b>, c'est l'adresse unique pour contacter les auteurs.<br/>
 
 <h2 id="delai">Quel est le délai de mise à jour des emplois du temps ?</h2>
-Les emplois du temps sont mis à jour toutes les heures. Si vous trouvez que ce délai n'est pas adapté, <a href="#contact">contactez-nous</a>.
+Les emplois du temps sont mis à jour très régulièrement&nbsp;:
+<b>toutes les deux heures</b> pour les semaines courante et suivante, toutes les trois heures pour les autres.
+ Si vous trouvez que ce délai n'est pas adapté, <a href="#contact">contactez-nous</a>.
 
 <h2 id="fonctionnement">Comment fonctionne votre site ?</h2>
 Ce site a été développé avec le framework PHP <?php echo link_to('Symfony', 'http://www.symfony-project.org/') ?>.
@@ -31,5 +38,5 @@ Alors votre profil nous intéresse ! <a href="#contact">Contactez-nous</a> très
 
 <h2 id="sources">Où sont les sources de votre logiciel ?</h2>
 Notre logiciel n'est pas (encore) un Logiciel Libre. L'ouverture des sources d'un tel logiciel ne nous apparaît pas comme prioritaire,
-maix exprimez votre demande : il y a de grande chance que nous publierons les sources.
+mais exprimez votre demande : il y a de grande chance que nous publierons les sources.
 
