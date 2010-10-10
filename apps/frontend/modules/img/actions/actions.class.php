4af9acf30fe8d4a7f28483d7a41658a92c711847
t.helleboid <t.helleboid@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-10-10 12:21:34 +0000

    Réorganisation routes Séparation de l'action pour les images dans un module img
    git-svn-id: https://svn.iariss.fr/edt@174 81824da9-04c7-4f20-bb1d-81339a13f318

---
+++ apps/frontend/modules/img/actions/actions.class.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * edt actions.
+ *
+ * @package    edt
+ * @subpackage edt
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class imgActions extends sfActions
+{
+
+  /**
+    Display the gif of the week
+  */
+  public function executeImg(sfWebRequest $request)
+  {
+    $filiere = $request->getParameter('filiere');
+    $promo = $request->getParameter('promo');
+    
+    $semaine = intval($request->getParameter('semaine', AdeTools::getSemaineNumber()));
+
+    $adeImage = new AdeImage(
+      array(array('filiere' => $filiere, 'promo' => $promo )),
+      array('idPianoWeek' => $semaine)
+    );
+    $adeImage->updateImage();
+
+    $filepath = sfConfig::get('sf_web_dir').$adeImage->getWebPath();
+
+    // Set content and exit
+    $this->getResponse()->setContentType('image/gif');
+    $this->getResponse()->setHttpHeader('Content-Length', filesize($filepath));
+    $this->getResponse()->setHttpHeader('Last-Modified', gmdate('D, d M Y H:i:s', filemtime($filepath)).' GMT');
+    // The image can be cached by proxy and browser's cache, during at most 3600 seconds
+    $this->getResponse()->addCacheControlHttpHeader('public');
+    $this->getResponse()->addCacheControlHttpHeader('max-age', '3600');
+    // Debug info
+    if(strlen($adeImage->getError()))
+      $this->getResponse()->setHttpHeader('X-Edt-error', $adeImage->getError());
+    // Send content
+    $this->getResponse()->setContent(file_get_contents($filepath));
+
+    // Send only the content without the layout
+    return sfView::NONE;
+    
+    // If you want to debug, comment the previous line and uncomment the following
+    // $this->setTemplate('index');
+  }
+
+}
