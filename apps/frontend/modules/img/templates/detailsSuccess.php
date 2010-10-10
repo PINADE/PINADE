c788936a9c58e11229308b5b2bf96085841701c1
t.helleboid <t.helleboid@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-10-10 13:34:29 +0000

    Ajout template pour details
    git-svn-id: https://svn.iariss.fr/edt@177 81824da9-04c7-4f20-bb1d-81339a13f318

---
+++ apps/frontend/modules/img/templates/detailsSuccess.php
@@ -0,0 +1,8 @@
+Information sur l'image<br/>
+<ul>
+  <li>Filière :  <?php echo $filiere ?></li>
+  <li>Promo :    <?php echo $promo ?></li>
+  <li>Semaine :  <?php echo $semaine ?></li>
+  <li>Filepath : <?php echo $filepath ?></li>
+  <li>Dernière modification : <?php echo strftime("%a %d %b %Y %H:%M:%S", $mtime) ?></li>
+</ul>
