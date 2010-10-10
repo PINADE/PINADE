72b1fc1d388cbfac45d42f0bb6011015d0e86cc3
t.helleboid <t.helleboid@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-10-10 13:38:14 +0000

    Rouge qd l'image n'existe pas
    git-svn-id: https://svn.iariss.fr/edt@178 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/modules/img/templates/detailsSuccess.php
+++ apps/frontend/modules/img/templates/detailsSuccess.php
@@ -1,8 +1,8 @@
 Information sur l'image<br/>
 <ul>
-  <li>Filière :  <?php echo $filiere ?></li>
-  <li>Promo :    <?php echo $promo ?></li>
-  <li>Semaine :  <?php echo $semaine ?></li>
-  <li>Filepath : <?php echo $filepath ?></li>
-  <li>Dernière modification : <?php echo strftime("%a %d %b %Y %H:%M:%S", $mtime) ?></li>
+  <li>Filière :  <b><?php echo $filiere ?></li></b>
+  <li>Promo :    <b><?php echo $promo ?></li></b>
+  <li>Semaine :  <b><?php echo $semaine ?></li></b>
+  <li>Filepath : <b <?php echo ($mtime == 0) ? 'style="color:red"' : '' ?>><?php echo $filepath ?></b></li>
+  <li>Dernière modification : <b><?php echo strftime("%a %d %b %Y %H:%M:%S", $mtime) ?></b></li>
 </ul>
