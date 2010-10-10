c861e59b94a186923e72b395b768b5f6369f5f49
t.helleboid <t.helleboid@81824da9-04c7-4f20-bb1d-81339a13f318>
2010-10-10 23:13:45 +0000

    Correction check à cause de iariss
    git-svn-id: https://svn.iariss.fr/edt@190 81824da9-04c7-4f20-bb1d-81339a13f318

--- apps/frontend/modules/cron/actions/actions.class.php
+++ apps/frontend/modules/cron/actions/actions.class.php
@@ -89,7 +89,10 @@ class cronActions extends sfActions
   */
   public function executeIdentifier(sfWebRequest $request)
   {
-    if (!($request->getParameter('debug') == '1' || in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))))
+    if (!($request->getParameter('debug') == '1'
+      || in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))
+      || gethostbyaddr($_SERVER['REMOTE_ADDR']) == "iariss")
+    )
     {
       $this->redirect('@homepage');
     }
