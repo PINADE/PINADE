2e64196a7d5eadd316f8c1474f42092f490a7332
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-03-08 08:43:09 +0100

    SUppression du code de suivi piwik

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -77,18 +77,5 @@
       </div>
     </div>
 
-    <!-- Piwik -->
-    <script type="text/javascript">
-      var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.iariss.fr/" : "http://piwik.iariss.fr/");
-      document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-    </script>
-    <script type="text/javascript">
-      try {
-        var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 9);
-        piwikTracker.trackPageView();
-        piwikTracker.enableLinkTracking();
-      } catch( err ) {}
-    </script><noscript><p><img src="http://piwik.iariss.fr/piwik.php?idsite=9" style="border:0" alt="" /></p></noscript>
-    <!-- End Piwik Tag -->
   </body>
 </html>
