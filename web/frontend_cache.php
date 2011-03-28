f5cb014d14ec19134c9d6e6f3f2b4f55a567af73
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-03-28 22:44:45 +0200

    Ajout d'une config du cache

---
+++ web/frontend_cache.php
@@ -0,0 +1,12 @@
+<?php
+// web/frontend_cache.php
+
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+{
+//  die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+}
+ 
+require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
+ 
+$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'cache', true);
+sfContext::createInstance($configuration)->dispatch();
