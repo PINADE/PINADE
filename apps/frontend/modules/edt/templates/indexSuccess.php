aa9c4eb67ca3cd2ce8ba7a79d643c59b9c810d15
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-04-18 14:46:32 +0200

    Mise en page de la page d'accueil : affichage en 3 colonnes

--- apps/frontend/modules/edt/templates/indexSuccess.php
+++ apps/frontend/modules/edt/templates/indexSuccess.php
@@ -1,18 +1,27 @@
 <?php slot('title'); ?>Accueil<?php end_slot() ?>
 <h1>
-  Bienvenue sur l'emploi du temps de l'ENSISA.
+  Bienvenue sur le site de <b>votre</b> emploi du temps.
 </h1>
 
-<h2>
-  Selectionnez votre filière:
-</h2>
 
+<h2>Les emplois du temps <i>officiels</i></h2>
 
-
-<ul >
+<ul style="-moz-column-count: 3;
+        -moz-column-gap: 20px;
+        -webkit-column-count: 3;
+        -webkit-column-gap: 20px;
+        column-count: 3;
+        column-gap: 20px;">
 <?php foreach($filieres as $filiere): ?>
   <li><?php echo image_tag("logos/".$filiere->getLogo(), "alt='logo ".$filiere."'");
    echo " ".link_to($filiere, '@filiere_index?filiere='.$filiere->getUrl()) ?>
+    <ul>
+    <?php foreach($filiere->getPromotions() as $promotion): ?>
+      <li>
+        <?php echo link_to($promotion, "@image?filiere=".$filiere->getUrl()."&promo=".$promotion->getUrl()."&semaine=") ?>
+      </li>
+    <?php endforeach ?>
+    </ul>
   </li>
 <?php endforeach ?>
 </ul>
