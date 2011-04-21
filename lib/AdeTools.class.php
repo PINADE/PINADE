c9ef36ba5bed239a1898450bcf8063157073c60a
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-04-22 01:54:10 +0200

    Migration du timestamp start dans la BDD

--- lib/AdeTools.class.php
+++ lib/AdeTools.class.php
@@ -1,19 +1,23 @@
 <?php
+/**
+ * edt Tools.
+ *
+ * @package    edt
+ * @subpackage Tools
+ * @author     M. Muré <m.mure@iariss.fr>, T. Helleboid <t.helleboid@iariss.fr>
+ */
 
 class AdeTools
 {
-
-
-  public static function getSemaineNumber($offset = 0)
+/*
+  public static function getSemaineNumber()
   {
-    // 2 jours et 6 heures avant le lundi, 0h (vendredi, 18h), on affiche la semaine suivante
-    // à appliquer si offset == 0 ?
-    if($offset == 0)
-      $Epoch_UHA = 1283119200 - (2*24 + 6)*60*60;
-    else
-      $Epoch_UHA = 1283119200;
 
-    return floor((time()- $Epoch_UHA)/(60*60*24*7)) + $offset;
+    // 2 jours et 6 heures avant le lundi, 0h (vendredi, 18h), on affiche la semaine suivante
+    $Epoch_UHA = 1283119200 - (2*24 + 6)*60*60;
+    
+
+    return floor((time()- $Epoch_UHA)/(60*60*24*7));
   }
   
   public static function getTimestamp($semaineADE)
@@ -22,5 +26,5 @@ class AdeTools
     
     return $Epoch_UHA + $semaineADE * (60*60*24*7);
   }
-  
+  */
 }
