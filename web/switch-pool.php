24bc137e01440d8927d4c8ebb20c2c5ad3132d4b
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-07-31 16:58:14 +0200

    Gestion du frontend plus fine avec le switch

--- web/switch-pool.php
+++ web/switch-pool.php
@@ -26,7 +26,3 @@ else
   die();
 }
 
-require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
-
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
-sfContext::createInstance($configuration)->dispatch();
