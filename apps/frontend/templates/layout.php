420805da2103dcec6cd9c1427af90198c2404268
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-08-03 08:54:46 +0200

    Remplacement de la description meta par le nom de l'Edt

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -4,8 +4,9 @@
   <head>
     <?php include_http_metas() ?>
     <?php include_metas() ?>
+    <meta name="description" content="Emplois du temps <?php include_partial('global/nom_edt') ?>" />
     <title><?php include_slot('title') ?> - Emploi du temps <?php include_partial('global/nom_edt') ?></title>
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="shortcut icon" href="/images/pinade.ico" />
     <?php include_stylesheets() ?>
     <?php include_partial('global/perso-css') ?>
     <link rel="stylesheet" href="/css/mobile.css" type="text/css" media="handheld, only screen and (max-device-width: 480px)" />
