84c3064079b08da15881b3656ba65258ccdb58e0
Théophile Helleboid - chtitux <chtitux@gmail.com>
2011-09-12 20:00:47 +0200

    Suppression du code de mise à jour de l'iCal dans l'actions du module ical

--- apps/frontend/modules/ical/actions/actions.class.php
+++ apps/frontend/modules/ical/actions/actions.class.php
@@ -11,25 +11,6 @@ class icalActions extends sfActions
 {
 
   /**
-    Update the iCal
-  */
-  public function executeUpdate(sfWebRequest $request)
-  {
-    $this->promotion = Doctrine_Core::getTable('Promotion')
-      ->createQuery('p')
-      ->leftJoin('p.Categorie c')
-      ->where('p.url = ? AND c.url = ?', array($request->getParameter('promo'),  $request->getParameter('categorie')))
-      ->execute()
-      ->getFirst();
-    $this->forward404Unless($this->promotion, "Pas de promotion trouvée");
-
-    $adeImage = new AdeImage($this->promotion, $semaine);
-
-    $adeImage->updateHtml();
-    $adeImage->updateIcal();
-    $this->path = $adeImage->getInfoPath();
-  }
-  /**
     Display the iCal
   */
   public function executeIcal(sfWebRequest $request)
